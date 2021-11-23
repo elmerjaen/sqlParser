2b1a8e7d2230dd0831ee6aad0bbf325f2d327ba2
Elmer Jaen <elmer.jaen@utp.ac.pa>
2021-11-22 20:58:44 -0500

    fix SELECT. Added COUNT and DROP

--- sqlParser.py
+++ sqlParser.py
@@ -6,10 +6,12 @@
 import re
 import os
 
-reserved_keywords = ('SELECT', 'FROM', 'DISTINCT', 'WHERE', 'BETWEEN', 'AND')
+reserved_keywords = (
+    'SELECT', 'FROM', 'DISTINCT', 'WHERE',
+    'BETWEEN', 'AND', 'COUNT', 'DROP', 'TABLE')
+
 keywords_in_data = []
 identifiers_in_data = []
-IDENTIFIERS = []
 
 def delete_repeated_identifier(identifiers, sql_string):
     unique_identifiers = []
@@ -19,19 +21,23 @@ def delete_repeated_identifier(identifiers, sql_string):
     
     unique_identifiers.append('\*') # * para la sentencia SELECT
     var = ""
-    table_name = ""
+    keywords = ""
     var += ',|'.join(unique_identifiers)
-    table_name += '|'.join(reserved_keywords)
-    evaluate_sql(var, table_name, sql_string)
+    keywords += '|'.join(reserved_keywords)
+    evaluate_sql(var, keywords, sql_string)
 
-def evaluate_sql(var, table_name, sql_string):
-    p_SELECT = re.compile('SELECT(['+var+']+)FROM(?!$|'+table_name+')')
-    # p_SELECT_DISTINCT = re.compile(r'SELECTDISTINCT('+ var +')FROM('+ var +')')
+def evaluate_sql(var, keywords, sql_string):
+    #p_SELECT = re.compile('SELECT(['+var+']+)FROM(?!$|\W|\w+\W|'+keywords+')')
+    p_SELECT = re.compile('SELECT([\w+,\w+]+|\*)FROM(?!$|\W|\w+\W|'+keywords+')')
+    p_COUNT = re.compile('SELECTCOUNT\(\w+\)FROM(?!$|\W|\w+\W|'+keywords+')')
+    p_DROP = re.compile('DROPTABLE(?!$|\W|\w+\W|'+keywords+')')
+    #p_SELECT = re.compile('SELECT(DISTINCT)?(['+var+']+)FROM(?!$|'+table_name+')')
+    #p_SELECT_DISTINCT = re.compile('SELECTDISTINCT(['+var+']+)FROM(?!$|'+table_name+')')
     #p_BETWEEN = re.compile(r'SELECT('+var+')FROM('+var+')WHERE('+var+')BETWEEN\dAND\d')
     
     os.system('cls')
     print('Pattern:', p_SELECT)
-    print(p_SELECT.match(sql_string))
+    print(p_SELECT.findall(sql_string))
     if p_SELECT.match(sql_string):
         print('La sentencia SQL es correcta.')
     else:
@@ -55,7 +61,8 @@ def classify(split_string, sql_string):
 def run():
     user_input = input("\nA continuación ingrese una sentencia SQL:\n")
     # get rid off the spaces in the string
-    split_string = re.split(r', |,| ', user_input)
+    split_string = re.split(',|, | ', user_input)
+    #split_string = re.split(' COUNT\(|\) | ', user_input)
     sql_string = ''.join(re.split(' ', user_input))
     classify(split_string, sql_string)
   
