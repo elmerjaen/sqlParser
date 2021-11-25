dcededb19799939cae1f24dcd3af6622d8d72ce1
Elmer Jaen <elmer.jaen@utp.ac.pa>
2021-11-24 22:26:36 -0500

    added SELECT DISTINCT and fixed other queries

--- sqlParser.py
+++ sqlParser.py
@@ -1,23 +1,27 @@
-# Analizador Léxico by Elmer Jaén
-
-# Get all the tokens of a string
-# Compare if the order of the tokens is a correct sql statement
+# Analizador Sintáctico
+# by Elmer Jaén, Louis Aguilar, Omar Flores
 
 import re
-import os
 
 keywords = 'SELECT|FROM|DISTINCT|WHERE|BETWEEN|AND|COUNT|DROP|TABLE'
 
 def evaluate_sql(sql_string):
 
-    p_SELECT = re.compile('(SELECT((?!$|\W|\w+\W|'+keywords+')|\*|\w+|\w+,\w+)FROM)(?!$|\W|\w+\W|'+keywords+')')
-    p_COUNT = re.compile('(SELECTCOUNT\(((?!$|\W|\w+\W|'+keywords+')|\w+)\)FROM)(?!$|\W|\w+\W|'+keywords+')')
-    p_DROP = re.compile('DROPTABLE(?!$|\W|\w+\W|'+keywords+')')
-    p_TRUNCATE = re.compile('TRUNCATETABLE(?!$|\W|\w+\W|'+keywords+')')
-    p_DELETE = re.compile('DELETEFROM(\w+)(?<!['+keywords+'])WHERE(\w+)(?<!['+keywords+'])(=|>|<|(=)*|<(=)*|<(>)*)(\'\w+\'|\d+)(?<!['+keywords+'])')
+    p_SELECT = re.compile('SELECT[\s](\w+|\*|\w+,[\s]\w+)(?<!['+keywords+'])[\s]FROM[\s]([a-zA-Z]+)(?<!['+keywords+'])([\s]WHERE[\s]([a-zA-Z]+)(?<!['+keywords+'])(=|>|<|(=)*|<(=)*|<(>)*)(\'\w+\'|\d+)(?<!['+keywords+']))*')
+    
+    p_SELECT_DISTINCT = re.compile('SELECT[\s]DISTINCT[\s](\w+)(?<!['+keywords+'])[\s]FROM[\s]([a-zA-Z]+)(?<!['+keywords+'])')
+    
+    p_COUNT = re.compile('SELECT[\s]COUNT\((\w+)(?<!['+keywords+'])\)[\s]FROM[\s]([a-zA-Z]+)(?<!['+keywords+'])')
+    
+    p_DROP = re.compile('DROP[\s]TABLE[\s]([a-zA-Z]+)(?<!['+keywords+'])')
+    
+    p_TRUNCATE = re.compile('TRUNCATE[\s]TABLE[\s]([a-zA-Z]+)(?<!['+keywords+'])')
+    
+    p_DELETE = re.compile('DELETE[\s]FROM[\s]([a-zA-Z]+)(?<!['+keywords+'])[\s]WHERE[\s]([a-zA-Z]+)(?<!['+keywords+'])(=|>|<|(=)*|<(=)*|<(>)*)(\'\w+\'|\d+)(?<!['+keywords+'])')
 
     patterns = {
         "SELECT": p_SELECT,
+        "SELECT_DISTINCT": p_SELECT_DISTINCT,
         "SELECT COUNT": p_COUNT,
         "DROP TABLE": p_DROP,
         "TRUNCATE TABLE": p_TRUNCATE,
@@ -25,21 +29,13 @@ def evaluate_sql(sql_string):
     }
 
     for key, value in patterns.items():
-        print(value.match(sql_string))
-        if key == "DELETE":
-            if value.fullmatch(sql_string):
-                return 1, key
-        else:
-            if value.match(sql_string):
-                return 1, key
+        if value.fullmatch(sql_string):
+            print(value.fullmatch(sql_string))
+            return 1, key
     return 0,0
 
 def run():
-    user_input = input("\nA continuación ingrese una sentencia SQL:\n")
-    # get rid off the spaces in the string
-    split_string = re.split(',|, | ', user_input)
-    #split_string = re.split(' COUNT\(|\) | ', user_input)
-    sql_string = ''.join(re.split(' ', user_input))
+    sql_string = input("\nA continuación ingrese una sentencia SQL:\n")
     evaluate_sql(sql_string)
   
 if __name__ == '__main__':
